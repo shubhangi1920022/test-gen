216f8a8ebe394fcc35e4fd0cc34c9e9d3bdefc29
shubhangi1920022 <shubhangidodake76@gmail.com>
2024-05-21 18:35:15 -0700

    first commit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export default async function DBConnection() {
   console.log("connecting")
   try {
-    await mongoose.connect("mongodb+srv://sanketnannaware21:BFUVkcom1A9fz6Dh@cluster0.eoefhna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("mongodb+srv://shubhangidodake32:EX7sJGcnmo5GLE1e@cluster0.mpbyjcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("connected to mongodb");
   } catch (error) {
     console.log("Error connecting to mongodb", error.message);
